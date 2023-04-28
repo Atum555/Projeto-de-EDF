f421a69b0a468f39b9774a80ad38ec07f6978563
Claudio Meireles <claudiomeireles555@gmail.com>
2023-04-28 18:31:39 +0100

    Clean Up Convert csv to Json

--- forms_data/convert_csv_to_json.py
+++ forms_data/convert_csv_to_json.py
@@ -4,18 +4,17 @@ import csv
 import json
 import sys
 
-
-def main():
+def get_raw_data():
     csv_file = sys.argv[1]
-    json_file = sys.argv[2]
-
     with open(csv_file, 'r') as file:
         reader = csv.DictReader(file, delimiter=',')
         data_raw = []
         for row in reader:
             # Convert the row to a JSON object and add it to the list
             data_raw.append(json.loads(json.dumps(row)))
+    return data_raw
 
+def get_data_questions(data_raw):
     data_questions = []
     for i in data_raw[0]:
         data_questions.append(i)
@@ -26,15 +25,30 @@ def main():
                 print("Questions don't match!")
                 exit(1)
             count += 1
+    return data_questions
 
+def get_data_answers(data_raw):
     data_answers = []
     for i in range(len(data_raw)):
         temp = []
         for j in data_raw[i]:
             temp.append(data_raw[i][j])
         data_answers.append(temp)
+    return data_answers
+
+    
+
+
+def main():
+    json_file = sys.argv[2]
+
+    data_raw = get_raw_data()
+    data_questions = get_data_questions(data_raw)
+    data_answers = get_data_answers(data_raw)    
 
     ## Continue Here
+
+    
 
     with open(json_file, 'w') as file:
         json.dump(data_raw, file, indent=4, ensure_ascii=False)
