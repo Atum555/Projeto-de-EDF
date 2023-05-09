cb7be3245b8863d1105349f0578f571757427a3b
Claudio Meireles <claudiomeireles555@gmail.com>
2023-05-09 12:03:35 +0100

    Graph Init

--- graphs_test/test_01.js
+++ graphs_test/test_01.js
@@ -1,40 +1,70 @@
-class _Graph {
-    constructor() {
-        this.canvas = _graphCanvas;
-
-    }
-    render() {
-        // Clear canvas
-        let ctx = this.canvas.getContext("2d");
-        ctx.fillStyle = "white";
-        ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-
-        // Draw Graph
-        
-    }
-}
-
 function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms));}
-
-let resizeGraphCanvas = () => {
-    _graphCanvas.width = Math.floor(_graphCanvas.parentElement.clientWidth) % 2 ? Math.floor(_graphCanvas.parentElement.clientWidth) - 1 : Math.floor(_graphCanvas.parentElement.clientWidth);
-    _graphCanvas.height = Math.floor(_graphCanvas.parentElement.clientHeight) % 2 ? Math.floor(_graphCanvas.parentElement.clientHeight) -1 : Math.floor(_graphCanvas.parentElement.clientHeight);
-    _graph.render();
-}
-window.onresize = resizeGraphCanvas;
-window.onkeydown = (e) => {
-    switch (e.code) {
-        case "KeyR": _graph.render(); break;
-    }
-};
 window.onload = () => {
-    resizeGraphCanvas();
+    _graph = new CanvasJS.Chart("chartContainer", {
+        animationEnabled: true,
+        theme: "light2", // "light1", "light2", "dark1", "dark2"
+        title:{
+            text: "Top Oil Reserves"
+        },
+        axisY: {
+            title: "Reserves(MMbbl)"
+        },
+        data: [{        
+            type: "column",  
+            showInLegend: true, 
+            legendMarkerColor: "grey",
+            legendText: "MMbbl = one million barrels",
+            dataPoints: [      
+                { y: 300878, label: "Venezuela" },
+                { y: 266455,  label: "Saudi" },
+                { y: 169709,  label: "Canada" },
+                { y: 158400,  label: "Iran" },
+                { y: 142503,  label: "Iraq" },
+                { y: 101500, label: "Kuwait" },
+                { y: 97800,  label: "UAE" },
+                { y: 80000,  label: "Russia" }
+            ]
+        }]
+    });
     _graph.render();
 };
 
-let _graphCanvas = document.getElementById("graphCanvas");
-let _graph = new _Graph();
+let _data;
+let _graph;
 
+const onDataLoaded = () => {
+    (function() {
+        const _select = document.getElementById("_graph_select");
+        for (let i = 5; i < _data["questions"].length; i++) {
+            let _option = document.createElement("option");
+            _option.value = i;
+            _option.innerHTML = _data["questions"][i];
+            _select.appendChild(_option);
+        }
+        const firstChild = _select.firstElementChild;
+        firstChild.classList.add('selected');
+        const changeEvent = new Event('change');
+    firstChild.dispatchEvent(changeEvent);
+    })();
+};
+
+const onGraphSelectChange = (event) => {
+    console.log(event.target.value);
+    console.log("it changed")
+}
+
+(function() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://94.61.210.193/edf/_data.json", true);
+    xhr.responseType = "json"; 
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            _data = xhr.response;
+            onDataLoaded();
+        }
+    };
+    xhr.send();
+})();
 
 /* ---INTERACTIONS---
 Redraw   {KeyR}
