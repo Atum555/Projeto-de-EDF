7cdd77779dbdbd39cb60dde441f57fc897613ba7
Claudio Meireles <claudiomeireles555@gmail.com>
2023-05-13 22:09:20 +0100

    Save Point

--- forms_data/get_data.py
+++ forms_data/get_data.py
@@ -55,17 +55,17 @@ def main():
                 {
                     "title": "Cursos",
                     "type": "Pie",
-                    "options": [    "Ciências e Tecnologias",    "Humanidades",    "Economia",    "Artes"]
+                    "options": ["Ciências e Tecnologias", "Humanidades", "Economia", "Artes"]
                 },
                 {
                     "title": "Especificas",
                     "type": "Pie",
-                    "options": [    "Química",    "Biologia",    "Física",    "Psicologia B",    "Inglês",    "Sociologia",    "História A",    "Geografia C",    "Oficina de Artes",    "Oficina Multimédia",    "Espanhol"]
+                    "options": ["Química", "Biologia", "Física", "Psicologia B", "Inglês", "Sociologia", "História A", "Geografia C", "Oficina de Artes", "Oficina Multimédia", "Espanhol"]
                 },
                 {
                     "title": "Peso",
                     "type": "ToDo",
-                    "options": [    "+150kg",    "120-150kg",    "100-120kg",    "90-100kg",    "85-90kg",    "80-85kg",    "75-80kg",    "70-75kg",    "65-70kg",    "60-65kg",    "55-60kg",    "50-55kg",    "45-50kg",    "40-45kg",    "20-40kg"]
+                    "options": ["+150kg", "120-150kg", "100-120kg", "90-100kg", "85-90kg", "80-85kg", "75-80kg", "70-75kg", "65-70kg", "60-65kg", "55-60kg", "50-55kg", "45-50kg", "40-45kg", "20-40kg"]
                 },
                 {
                     "title": "Altura",
@@ -74,55 +74,63 @@ def main():
                 {
                     "title": "Sexo",
                     "type": "Pie",
-                    "options": [    "Feminino",    "Masculino"]
+                    "options": ["Feminino", "Masculino"]
                 },
                 {
                     "title": "ToDo",
                     "type": "ToDo",
-                    "options": [    "1",    "2",    "3",    "4",    "5",    "6",    "7",    "8"]
+                    "options": ["1", "2", "3", "4", "5", "6", "7", "8"]
 
                 },
                 {
                     "title": "ToDo",
                     "type": "ToDo",
-                    "options": [    "Sim",    "Às vezes",    "Não"]
+                    "options": ["Sim", "Às vezes", "Não"]
                 },
                 {
                     "title": "ToDo",
                     "type": "ToDo",
-                    "options": [    "Sim",    "Mais ou menos",    "Não"]
+                    "options": ["Sim", "Mais ou menos", "Não"]
                 },
                 {
                     "title": "ToDo",
                     "type": "ToDo",
-                    "options": [    "Sim",    "Mais ou menos",    "Não"]
+                    "options": ["Sim", "Mais ou menos", "Não"]
                 },
                 {
                     "title": "ToDo",
                     "type": "ToDo",
-                    "options": [    "1",    "2",    "3",    "4",    "5",    "6"]
+                    "options": ["1", "2", "3", "4", "5", "6"]
                 },
                 {
                     "title": "ToDo",
                     "type": "ToDo",
-                    "options": [    "1",    "2",    "3",    "4",    "5",    "6",    "7",    "8",    "9",    "10"]
+                    "options": ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
                 },
                 {
                     "title": "ToDo",
                     "type": "ToDo",
-                    "options": [    "Sim e respeito-o",    "Sim mas não o respeito-o",    "Mais ou menos",    "Não"]
+                    "options": ["Sim e respeito-o", "Sim mas não o respeito-o", "Mais ou menos", "Não"]
                 },
                 {
                     "title": "ToDo",
-                    "type": "ToDo"
+                    "type": "ToDo",
+                    "options": ["Sim e sigo um plano alimentar", "Sim mas não sigo um plano alimentar rigoroso", "Não mas sigo um plano alimentar", "Não e não sigo qualquer tipo de plano alimentar"]
                 },
                 {
                     "title": "ToDo",
-                    "type": "ToDo"
+                    "type": "ToDo",
+                    "options": ["Veganismo", "Vegetarianismo", "Não"]
                 },
                 {
                     "title": "ToDo",
-                    "type": "ToDo"
+                    "type": "ToDo",
+                    "options": ["1-2", "3-5", "6-10", "+10", "Apenas ocasiões expeciais ou nunca."]
+                },
+                {
+                    "title": "TBM",
+                    "type": "TBM",
+                    "options": ["Sim", "Não"]
                 },
                 {
                     "title": "TBM",
@@ -130,67 +138,78 @@ def main():
                 },
                 {
                     "title": "TBM",
-                    "type": "TBM"
+                    "type": "TBM",
+                    "options": ["Coletivo", "Individual"]
                 },
                 {
                     "title": "TBM",
-                    "type": "TBM"
+                    "type": "TBM",
+                    "options": ["1", "2", "3", "4", "5", "6", "7"]
                 },
                 {
                     "title": "TBM",
-                    "type": "TBM"
+                    "type": "TBM",
+                    "options": ["Sim", "Não"]
                 },
                 {
                     "title": "TBM",
-                    "type": "TBM"
+                    "type": "TBM",
+                    "options": ["1", "2", "3", "4", "5", "6", "7"]
                 },
                 {
                     "title": "TBM",
-                    "type": "TBM"
+                    "type": "TBM",
+                    "options": ["<30min", "30min - 1h", "1h - 2h", ">2h"]
                 },
                 {
                     "title": "TBM",
-                    "type": "TBM"
+                    "type": "TBM",
+                    "options": ["Sim, mais do que uma...", "Sim", "Não"]
                 },
                 {
                     "title": "TBM",
-                    "type": "TBM"
+                    "type": "TBM",
+                    "options": ["pouco ou nenhum", "leve 1-3 dias por semana", "de intensidade moderada 3-5 dias por semana", "intenso 6-7 dias por semana", "muito intenso 6-7 dias por semana"]
                 },
                 {
                     "title": "TBM",
-                    "type": "TBM"
+                    "type": "TBM",
+                    "options": ["<5h", "5h - 6h", "6h - 7h", "7h - 8h", "8h - 9h", ">9h"]
                 },
                 {
                     "title": "TBM",
-                    "type": "TBM"
+                    "type": "TBM",
+                    "options": ["<5h", "5h - 6h", "6h - 7h", "7h - 8h", "8h - 9h", "9h - 10h", ">10h"]
                 },
                 {
                     "title": "TBM",
-                    "type": "TBM"
+                    "type": "TBM",
+                    "options": ["Sim", "Às vezes", "Não"]
                 },
                 {
                     "title": "TBM",
-                    "type": "TBM"
+                    "type": "TBM",
+                    "options": ["Sim", "Às vezes", "Não"]
                 },
                 {
                     "title": "TBM",
-                    "type": "TBM"
+                    "type": "TBM",
+                    "options": ["Sim", "Às vezes", "Não"]
                 },
                 {
                     "title": "TBM",
-                    "type": "TBM"
+                    "type": "TBM",
+                    "options": ["Sim", "Às vezes", "Não"]
                 },
                 {
                     "title": "TBM",
-                    "type": "TBM"
+                    "type": "TBM",
+                    "options": ["Sim", "Às vezes", "Não"]
                 },
                 {
                     "title": "TBM",
-                    "type": "TBM"
-                },
-                {
-                    "title": "TBM",
-                    "type": "TBM"
+                    "type": "TBM",
+                    "options": ["Sim", "Às vezes", "Não"]
                 }
             ]
         },
